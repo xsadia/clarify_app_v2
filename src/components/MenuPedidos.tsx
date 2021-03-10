8db5c547107c4e82c08f5c9bd62f81f52fc39039
xsadia <lipearthur@hotmail.com>
2021-03-10 16:41:09 -0300

    android build

---
+++ src/components/MenuPedidos.tsx
@@ -0,0 +1,101 @@
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+} from '@ionic/react';
+
+import {
+  logInOutline,
+  logInSharp,
+  statsChartOutline,
+  statsChartSharp,
+  pricetagsOutline,
+  pricetagsSharp
+} from 'ionicons/icons';
+
+import { useLocation } from 'react-router-dom';
+
+import './Menu.css';
+
+interface AppPage {
+  url: string;
+  iosIcon: string;
+  mdIcon: string;
+  title: string;
+}
+
+/* {
+  title: 'home',
+  url: '/',
+  iosIcon: homeOutline,
+  mdIcon: homeSharp
+}, */
+
+const appPages: AppPage[] = [
+  {
+    title: 'Login',
+    url: '/login',
+    iosIcon: logInOutline,
+    mdIcon: logInSharp
+  },
+  {
+    title: 'Dashboard',
+    url: '/dashboard',
+    iosIcon: statsChartOutline,
+    mdIcon: statsChartSharp
+  },
+  {
+    title: 'Pedidos',
+    url: '/pedidos',
+    iosIcon: pricetagsOutline,
+    mdIcon: pricetagsSharp
+  }
+];
+
+interface Props {
+  email: string;
+  firstName: string;
+}
+
+const MenuPedido: React.FC<Props> = ({email}) => {
+  const location = useLocation();
+
+  return (
+    <IonMenu contentId="main" type="overlay">
+      <IonContent>
+        <IonList id="inbox-list">
+          <IonListHeader>Inbox</IonListHeader>
+          <IonNote>{email}</IonNote>
+          {appPages.map((appPage, index) => {
+            return (
+              <IonMenuToggle key={index} autoHide={false}>
+                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+                  <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                  <IonLabel>{appPage.title}</IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+        </IonList>
+
+        {/* <IonList id="labels-list">
+          <IonListHeader>Labels</IonListHeader>
+          {labels.map((label, index) => (
+            <IonItem lines="none" key={index}>
+              <IonIcon slot="start" icon={bookmarkOutline} />
+              <IonLabel>{label}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList> */}
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default MenuPedido;
